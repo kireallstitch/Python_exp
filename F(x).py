2ef8a887299eb2e0e29533f1f643b43fcb1f9d6d
kireallstitch <stitch-man@yandex.ru>
2023-01-03 16:55:49 +0700

    exp

--- F(x).py
+++ F(x).py
@@ -1,8 +1,11 @@
-+def F(x):
+def F(x):
     return x ** 2
+
+
 for x in range(-10, 10):
-    print('Для F('+str(x) + ') = '+str(F(x)))
+    print('Для F(' + str(x) + ') = ' + str(F(x)))
 i = -3
 while i < 3:
     print('Для F(' + str(i) + ') = ' + str(F(i)))
-    i = i + 0.1
+    i = i + 2
+
