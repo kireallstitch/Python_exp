18772f22f222dbeb450bfb43b7b3dc5a38a35929
kireallstitch <stitch-man@yandex.ru>
2023-01-03 18:56:43 +0700

    exp

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-print('enter number')
-number = int(input())
-
-def collatz(number):
-    if number % 2 == 0:
-        number = int(number // 2)
-
-    elif number % 2 == 1:
-        number = int(3 * number + 1)
-    return number
-while number != 1:
-    print(number)
-    number = collatz(number)
-    if number == 1:
-        print(number)
-        break
+for i in [0, 1, 2, 3]:
+    print(i)
