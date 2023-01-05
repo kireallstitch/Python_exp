572aaa8502042ef74af634effd596dd62b7c68ed
kireallstitch <stitch-man@yandex.ru>
2023-01-05 21:38:53 +0700

    Добавил костыли для фунций Сочинил реализацию Нужно сделать 3 фунции для основания стен и крыши

--- Лекции/5/house.py
+++ Лекции/5/house.py
@@ -4,6 +4,7 @@ def main():
 
     draw_house(x, y, width, height)
 
+
 def draw_house(x, y, width, height):
     """
     Нарисовать домик ширины width и высоты height от опорной точки(x, y),
@@ -21,9 +22,33 @@ def draw_house(x, y, width, height):
     roof_height = height - foundation_height - wall_height
 
     draw_house_foundation(x, y, width,foundation_height)
-    draw_house_walls(x, y - foundation_height,wall_height, wall_width)
-    draw_house_roof(x, y - foundation_height - wall_height, roof_height)
-    
+    draw_house_walls(x, y - foundation_height, wall_height, wall_width)
+    draw_house_roof(x, y - foundation_height - wall_height, width, roof_height)
 
+
+def draw_house_foundation(x, y, width,height):
+    """
+    Нарисовать основание домика ширины width и высоты height от опорной точки(x, y),
+    которая находится в середине нижней точки фундамента.
+    :param x: коорд. x середины фундамента
+    :param y: коорд. y  низа фундамента
+    :param width: полная ширина фундамента (фундамент и вылет крыши вкл.)
+    :param height: полная высота фундамента
+    :return: None
+    """
+    print('Типа рисую основание...', x, y, width, height)
+    pass
+
+
+def draw_house_walls(x, y, width,height):
+    print('Типа рисую стены...', x, y, width, height)
+    pass
+
+
+def draw_house_roof(x, y, width,height):
+    print('Типа рисую крышу...', x, y, width, height)
+    pass
+ 
+ 
 
 main()
