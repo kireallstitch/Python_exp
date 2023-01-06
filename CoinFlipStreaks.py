d84675c3b11f1c130ac20ba26a8b610af822481b
kireallstitch <stitch-man@yandex.ru>
2023-01-06 22:03:35 +0700

    Написать программу которая подсчитает стрики из 6 орлов или решек из 10 000 попыток.

--- CoinFlipStreaks.py
+++ CoinFlipStreaks.py
@@ -1,1 +1,7 @@
+import random
+numberOfStreaks = 0
+for experimentNumber in range(10000):
+    # Код, создающий список из 100 значений 'heads' или 'tails'
 
+    # Код, который проверяет, есть ли полоса из 6 орлов или решек подряд.
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
