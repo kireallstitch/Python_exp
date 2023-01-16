a11beee9770af1e4a62eb394b0473ee656e3b591
kireallstitch <stitch-man@yandex.ru>
2023-01-16 11:27:41 +0700

    я сделаль

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-def find Smallest(a r r):
-smallest = arr[0]
-С· · · ···
-smallest_index = 0
-с""""".
-for i in range(l, len(arr)):
-if arr[i] < smallest:
-smallest = arr[i]
-smallest_index = i
-return smallest_index
+ran = int(input('Range? >'))
+verif = ran / 2
+array = [0]*ran
+for i in range(ran):
+    item = int(input("Data = >"))
+    array[i] = item
+if (isinstance(verif, float)):
+    print(True)
+    midpoint = int(verif + 1.5)
+    median = array[(midpoint)]
+print(median)
