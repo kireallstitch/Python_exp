cfcfb4b7e1f6aeea31de5eeb040bcb839b82023a
kireallstitch <stitch-man@yandex.ru>
2023-01-17 18:00:42 +0700

    я сделаль

--- BoringStuff/catnapping.py
+++ BoringStuff/catnapping.py
@@ -1,17 +1,6 @@
-print('''Dear 
-Alice,
-Eve'
-s 
-cat 
-has
- been
-  arrested 
-  for
-   catnapping, 
-   cat burglary,
-    and
-     extortion.
-     
-
-Sincerely,
-Bob''')
+print('How are you?')
+feeling = input()
+if feeling.lower() == 'great':
+    print('I feel great too.')
+else:
+    print('I hope the rest of your day is good.')
