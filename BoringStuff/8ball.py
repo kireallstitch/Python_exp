40f8730fe43835f2964e496dc29200981ec913d0
kireallstitch <stitch-man@yandex.ru>
2023-07-17 12:44:05 +0700

    я сделаль

--- BoringStuff/8ball.py
+++ BoringStuff/8ball.py
@@ -3,29 +3,29 @@ import sys
 import time
 import keyboard
 
-print('Магический Шар приветсвует тебя')
+print('Магический Шар приветствует тебя')
 print('Нажмите на кнопку ПРОБЕЛ, что бы потрясти шар')
 print('НАЖМИТЕ НА КНОПКУ ESC, ЧТО БЫ ВЫЙТИ')
 
 
-def get_answer(answerNumber):
-    if answerNumber == 1:
+def get_answer(answernumber):
+    if answernumber == 1:
         return 'Это несомненно!'
-    elif answerNumber == 2:
+    elif answernumber == 2:
         return 'Это однозначно так!'
-    elif answerNumber == 3:
+    elif answernumber == 3:
         return 'Да!'
-    elif answerNumber == 4:
+    elif answernumber == 4:
         return 'Ответ не ясен! Спроси ещё раз!'
-    elif answerNumber == 5:
+    elif answernumber == 5:
         return 'Спроси попозже!'
-    elif answerNumber == 6:
+    elif answernumber == 6:
         return 'Мой ответ нет!'
-    elif answerNumber == 7:
+    elif answernumber == 7:
         return 'Сконцентрируйся! И спроси ещё!'
-    elif answerNumber == 8:
+    elif answernumber == 8:
         return 'Прогноз не утешительный!'
-    elif answerNumber == 9:
+    elif answernumber == 9:
         return 'Очень сомневаюсь!'
 
 
@@ -37,7 +37,7 @@ while True:
 
         print(f'{get_answer(random.randint(1, 9)):#^50}')
         time.sleep(0.5)
-        print('Магический шар приветсвует тебя')
+        print('Магический шар приветствует тебя')
         print('Нажмите на кнопку ПРОБЕЛ, что бы потрясти шар')
         print('Нажмите на кнопку esc, что бы выйти')
         continue
